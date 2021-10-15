20919d479fc532060e2a1e4adec1c3c3effac615
Rahul Jain <71962320+Rahul-jain-01@users.noreply.github.com>
2021-10-15 19:33:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+  document.getElementById('inst').addEventListener('click', (ev) => {
+    let url = 'second.html';
+     window.open(url, '_self',);
+  });
+
+  document.getElementById('contact').addEventListener('click', (ev) => {
+    let url = 'four.html';
+    window.open(url, '_self');
+  });
+  document.getElementById('cs').addEventListener('click', (ev) => {
+    let url = 'CSE.html';
+    window.open(url, '_self');
+  });
+  document.getElementById('civil').addEventListener('click', (ev) => {
+    let url = 'civil.html';
+     window.open(url, '_self',);
+  });
+  document.getElementById('ece').addEventListener('click', (ev) => {
+    let url = 'ECE.html';
+     window.open(url, '_self',);
+  });
+  document.getElementById('mech').addEventListener('click', (ev) => {
+    let url = 'Mech.html';
+     window.open(url, '_self',);
+  });
+ 
+ 
